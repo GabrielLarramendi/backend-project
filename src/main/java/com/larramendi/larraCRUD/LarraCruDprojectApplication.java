c64659ddcb4a820c43a41dc20ca1bbf7a7e32902
Gabriel Larramendi <larramendiga@gmail.com>
2023-05-22 22:17:24 -0300

    First commit

---
+++ src/main/java/com/larramendi/larraCRUD/LarraCruDprojectApplication.java
@@ -0,0 +1,13 @@
+package com.larramendi.larraCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarraCruDprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LarraCruDprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/larramendi/larraCRUD/LarraCruDprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larramendi.larraCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarraCruDprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
